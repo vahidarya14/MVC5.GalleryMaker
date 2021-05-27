c3c74e063201777ef75e266ba00ead8d5708340f
a <tohellhi@gmail.com>
2021-05-27 17:04:09 +0330

    a

--- GalleryMaker/Views/Gallery/GalleryController.cs
+++ GalleryMaker/Views/Gallery/GalleryController.cs
@@ -9,38 +9,31 @@ using GalleryMaker.Models;
 using Microsoft.AspNet.Identity;
 
 namespace GalleryMaker.Views.Gallery
-{
+{ 
+    [Authorize]
     public class GalleryController : Controller
     {
         AppDbContext _db = new AppDbContext();
 
         public long UserId => long.Parse(User.Identity.GetUserId());
 
-        public static List<Layout> AllLayout = new List<Layout>();
-        private string path;
-        public GalleryController()
-        {
-            //TempLists.Init();
-        }
 
         private void InitViews()
         {
-
-
-            var cats = new List<CatGroupedModel>();
-            foreach (var cat in TempLists.Cats)
-            {
-                var ls = TempLists.Layouts.Where(a => a.Cat.Id == cat.Id);
-                var cg = new CatGroupedModel();
-                cg.CatName = cat.Name;
-                cg.Layouts = ls.ToList();
-                cats.Add(cg);
-            }
-            ViewData["AllLayout"] = cats;
+            //var cats = new List<CatGroupedModel>();
+            //foreach (var cat in TempLists.Cats)
+            //{
+            //    var ls = TempLists.Layouts.Where(a => a.Cat.Id == cat.Id);
+            //    var cg = new CatGroupedModel();
+            //    cg.CatName = cat.Name;
+            //    cg.Layouts = ls.ToList();
+            //    cats.Add(cg);
+            //}
+            //ViewData["AllLayout"] = cats;
             ViewData["SlidShowImages"] = new Uploader(_db).LoadImages(UserId);
         }
 
-        [Authorize]
+      
         public ActionResult Index(int? id)
         {
             InitViews();
@@ -61,7 +54,7 @@ namespace GalleryMaker.Views.Gallery
             return View("Index", model);
         }
 
-        [Authorize]
+ 
         [Route("PhotoMgmt")]
         public ActionResult PhotoMgmt()
         {
@@ -69,9 +62,7 @@ namespace GalleryMaker.Views.Gallery
             return View();
         }
 
-        [Authorize]
-        [HttpPost]
-        [ValidateInput(false)]
+        [HttpPost,ValidateInput(false)]
         public async Task<JsonResult> SaveLayout(Project a)
         {
             a.HtmlString = a.HtmlString.Replace("\n\n", "").Trim();
@@ -100,8 +91,6 @@ namespace GalleryMaker.Views.Gallery
             return Json(a);
         }
 
-
-        [Authorize]
         [HttpPost]
         public async Task<ActionResult> UploadImages(ICollection<HttpPostedFileBase> files)
         {
@@ -118,7 +107,6 @@ namespace GalleryMaker.Views.Gallery
             return RedirectToAction("PhotoMgmt");
         }
 
-        [Authorize]
         [HttpPost]
         public async Task<ActionResult> RemoveImage(string name)
         {
